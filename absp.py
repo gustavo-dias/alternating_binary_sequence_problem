fffcfa39fc4c613a11a4a611e8db26e1d654e4aa
Gustavo Dias <gustavodias.po@gmail.com>
2022-08-30 09:03:33 -0300

    Remove trailing whitespaces.

--- absp.py
+++ absp.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-The module implements a performance comparison between Mathematical Programming 
-and an O(n) algorithm when solving the Alternating Binary Sequence Problem 
+The module implements a performance comparison between Mathematical Programming
+and an O(n) algorithm when solving the Alternating Binary Sequence Problem
 (ABSP).
 
 
@@ -33,7 +33,7 @@ class Instance ():
             containing exclusevily zeros and/or ones.
         """
         size = int(len(arg.strip()))
-        
+
         try:
             sequence = [int(bit) for bit in list(arg.strip())]
             for i in range(size):
@@ -42,19 +42,19 @@ class Instance ():
         except ValueError:
             print("Warning: please input binary digits only.\n")
             return None
-        
+
         return cls(size, sequence)
-        
+
     def __str__(self):
         return "size = {0}, sequence = {1}".format(self.size, self.sequence)
 
 
 class Solution ():
     """Class that represents solutions to the ABSP."""
-    
-    def __init__(self, 
+
+    def __init__(self,
                  variables : list,
-                 num_flips: int = 0, 
+                 num_flips: int = 0,
                  runtime: float = 0.0,
                  ):
         self.num_flips = num_flips
@@ -73,14 +73,14 @@ class Solution ():
 
 class AlgorithmType (IntEnum):
     """Class that implements an enumeration of types of algorithms."""
-    
+
     b_and_b = 1
     algo_on = 2
 
 
 class Algorithm (ABC):
     """Abstract class that represents solution algorithms."""
-    
+
     @abstractmethod
     def run ():
         pass
@@ -88,25 +88,25 @@ class Algorithm (ABC):
 
 class Problem ():
     """Class that represents ABSP problems."""
-    
+
     def __init__(self, instance : Instance):
         self.instance = instance
         self.solution = None
- 
+
     @property
     def mathprog_model (self):
         A = self.instance.sequence
         n = self.instance.size
         N = range(n)
-    
+
         mp = picos.Problem()
-    
+
         x = picos.BinaryVariable('x', n)
         y = picos.BinaryVariable('y', n)
         z = picos.BinaryVariable('z', n)
-    
+
         mp.set_objective('min', picos.sum([x[i] for i in N]))
-    
+
         for i in N:
             mp.add_constraint(y[i] >= A[i] - x[i])
             mp.add_constraint(y[i] >= -A[i] + x[i])
@@ -131,7 +131,7 @@ class Problem ():
             self.solution = AlgoON(self.instance).run()
         else:
             print("No algorithm selected.\n")
-            
+
         if report and self.solution is not None:
             self.solution.report
 
@@ -139,25 +139,25 @@ class Problem ():
 class BranchAndBound (Algorithm):
     """
         Class that represents Branch and Bound algorithms.
-    
+
         The class does not implement a B&B algorithm per see, it rather calls \
         third-party implementations.
     """
     def __init__(self, model, solver):
         self.model = model
         self.solver = solver
-    
+
     def run (self):
         """Run the B&B algorithm and return a Solution."""
-        
+
         try:
             sol = self.model.solve(solver=self.solver, verbosity=False)
-            
+
             float_values = sol.primals.values()
             int_values = [
                 [int(bit) for bit in entry] for entry in list(float_values)
                 ]
-            
+
             return Solution(int_values, int(sol.value), sol.searchTime)
 
         except Exception as e:
@@ -168,16 +168,16 @@ class BranchAndBound (Algorithm):
 
 class AlgoON (Algorithm):
     """CLass that implements the O(n) algorithm."""
-    
+
     def __init__(self, instance: Instance):
         self.instance = instance
 
     @staticmethod
     def get_alternating_sequences_of_size(arg : int):
         """Return lists storing the two alternating sequences of size arg."""
-        
+
         seq_1, seq_2 = [], []
-        
+
         for i in range(arg):
             if i % 2 == 0:
                 seq_1.append(0)
@@ -185,19 +185,19 @@ class AlgoON (Algorithm):
             else:
                 seq_1.append(1)
                 seq_2.append(0)
-                
+
         return seq_1, seq_2
-    
+
     def run (self):
         """Run the algorithm O(n) and return a Solution."""
-        
+
         start = time()
-    
+
         seq_1, seq_2 = self.get_alternating_sequences_of_size(
             self.instance.size
             )
         flips_to_seq_1, flips_to_seq_2 = [], []
-        
+
         # loop over each of the input sequence's n bits, thus O(n)
         for i in range(self.instance.size):
             # compare with both sequences and store the map of required flips;
@@ -208,23 +208,23 @@ class AlgoON (Algorithm):
             if self.instance.sequence[i] != seq_1[i]:
                 # need to flip to reach seq_1
                 flips_to_seq_1.append(1)
-    
+
                 # no need to flip to reach seq_2
                 flips_to_seq_2.append(0)
             else:
                 # no need to flip to reach seq_1
                 flips_to_seq_1.append(0)
-     
+
                 # need to flip to reach seq_2
                 flips_to_seq_2.append(1)
 
-        # select solution based on minimum number of required flips, given by 
+        # select solution based on minimum number of required flips, given by
         # the sum of the entries in the lists flips_to_seq_#, where # in {1,2}.
         if sum(flips_to_seq_1) <= sum(flips_to_seq_2):
             solution = Solution([flips_to_seq_1, seq_1], sum(flips_to_seq_1))
         else:
             solution = Solution([flips_to_seq_2, seq_2], sum(flips_to_seq_2))
-        
+
         end = time()
         solution.runtime = end-start
         return solution
@@ -237,7 +237,7 @@ if __name__ == "__main__":
         )
     )
     start = time()
-    
+
     if len(sys.argv) == 2:
         instance = Instance.create_from_string(sys.argv[1])
         if instance != None:
@@ -246,7 +246,7 @@ if __name__ == "__main__":
             ABSP.solve_using(AlgorithmType.algo_on, True)
     else:
         print("Usage: python alt_bin_seq_problem.py <binary sequence>\n")
-    
+
     end = time()
     print("{0}: End of execution; time elapsed: {1:.2f} seconds.".format(
         ctime(),
